fd4c86809c5bf21988c68e63b3eeddbe08162917
Shital Shah <shital@ShitalShah.com>
2017-05-18 17:36:31 -0700

    fixed IMU vel

--- AirLib/include/sensors/imu/ImuSimple.hpp
+++ AirLib/include/sensors/imu/ImuSimple.hpp
@@ -46,7 +46,7 @@ private: //methods
         Output output;
         const GroundTruth& ground_truth = getGroundTruth();
 
-        output.angular_velocity = ground_truth.kinematics->twist.angular;
+        output.angular_velocity = VectorMath::transformToBodyFrame(ground_truth.kinematics->twist.angular, ground_truth.kinematics->pose.orientation);
         output.linear_acceleration = ground_truth.kinematics->accelerations.linear - ground_truth.environment->getState().gravity;
         output.orientation = ground_truth.kinematics->pose.orientation;
 
